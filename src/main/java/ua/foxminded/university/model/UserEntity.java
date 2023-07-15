9dd8722d1a13e2a045f728b37e43616cbc914b6d
VasilMartsyniuk <martsyniukvas@gmail.com>
2023-07-15 20:04:46 +0200

    Commit for check!!

--- src/main/java/ua/foxminded/university/model/UserEntity.java
+++ src/main/java/ua/foxminded/university/model/UserEntity.java
@@ -1,7 +1,15 @@
 package ua.foxminded.university.model;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
